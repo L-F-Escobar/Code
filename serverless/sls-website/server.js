ae6982d06d8d6a35a614c009362162d767196bf6
LEscobar-Driver <lfescoba@uci.edu>
2020-05-25 13:10:57 -0700

    https up

--- serverless/sls-website/server.js
+++ serverless/sls-website/server.js
@@ -4,5 +4,5 @@ var app = express();
 // app.use(express.static(__dirname + '/local'));
 app.use(express.static(__dirname + '/frontend'));
 
-app.listen('3000');
-console.log('working on 3000');
+app.listen('8080');
+console.log('working on 8080');
